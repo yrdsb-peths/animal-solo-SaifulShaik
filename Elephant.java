42c98bd97b97d22eee1da5c61e4d16b2b9536b17
Saiful Shaik <Saifulshaik47@gmail.com>
2025-04-30 13:53:18 -0400

    1.1 - An Object on the Interface.
    Features:
    - An elephant on the interface.
    - Movement of the object displayed on interface.

---
+++ Elephant.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Elephant here.
+ * 
+ * @author Saiful Shaik
+ * @version April, 30, 2025
+ */
+public class Elephant extends Actor
+{
+    public Elephant() {
+        setImage("images/elephant.png");
+    }
+    public void act()
+    {
+        // Add your action code here.
+        move(1);
+    }
+}
