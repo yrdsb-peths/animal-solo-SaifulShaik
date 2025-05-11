34178cbeb16d16cbe8f1413fe62c9fdd64489e33
Saiful Shaik <Saifulshaik47@gmail.com>
2025-05-11 15:17:47 -0400

    2.8 Using a Simple Timer
    Features:
    - Timer for slower and better animations.

--- Elephant.java
+++ Elephant.java
@@ -12,6 +12,7 @@ public class Elephant extends Actor
     GreenfootImage[] idleRight = new GreenfootImage[8];
     GreenfootImage[] idleLeft = new GreenfootImage[8];
     String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     public Elephant() {
         for(int i = 0; i < idleRight.length; i++) {
             idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
@@ -24,11 +25,19 @@ public class Elephant extends Actor
             idleLeft[i].scale(100,100);
         }
         
+        animationTimer.mark();
+        
         setImage(idleRight[0]);
     }
     
     int imageIndex = 0;
     public void animateElephant() {
+        if(animationTimer.millisElapsed() < 100) {
+            return;
+        }
+        
+        animationTimer.mark();
+        
         if(facing.equals("right")) {
             setImage(idleRight[imageIndex]);
             imageIndex = (imageIndex + 1) % idleRight.length;
